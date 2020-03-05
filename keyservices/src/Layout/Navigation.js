9f34810ed36e5184d3d0319352a0b1662c86f34e
shahin321 <shahinchoudhurypro@gmail.com>
2020-03-05 20:40:11 +0100

    Nav link navigation #6

--- keyservices/src/Layout/Navigation.js
+++ keyservices/src/Layout/Navigation.js
@@ -1,27 +1,19 @@
 import React, {Component} from 'react';
 import { NavLink } from 'react-router-dom';
-import {Navbar, Nav} from 'react-bootstrap';
+import {Navbar, Nav, NavItem, Glyphicon, Grid, Row, Col} from 'react-bootstrap';
 
 export class Navigation extends Component {
     render() {
         return(
 
-            <Navbar bg="transparent" expand="lg" className="text-center mt-4 mb-4">
+          <Navbar bg="transparent" expand="lg" className="justify-content-center mt-4 mb-4">
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="justify-content-center width=100">
-                  <Nav.Item> 
+              <Nav>
                     <NavLink className="d-inline p-2 bg-dark text-white" to="/">Accueil</NavLink>
-                  </Nav.Item>
-                  <Nav.Item>
                     <NavLink className="d-inline p-2 bg-dark text-white" to="/keyservicesPresentation">Keyservices</NavLink>
-                  </Nav.Item>
-                  <Nav.Item>
                     <NavLink className="d-inline p-2 bg-dark text-white" to="/contact">Contact</NavLink>
-                  </Nav.Item>
-                  <Nav.Item>
                     <NavLink className="d-inline p-2 bg-dark text-white" to="/monespace">Mon Espace</NavLink>
-                  </Nav.Item>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
