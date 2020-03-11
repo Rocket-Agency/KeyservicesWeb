4313fbbeb9af6ba0e0a90d16d6fd103fb0318db2
shahin321 <shahinchoudhurypro@gmail.com>
2020-03-11 13:34:06 +0100

    Style css image et boutons suite #13

--- keyservices/src/components/Pages/Home.js
+++ keyservices/src/components/Pages/Home.js
@@ -13,8 +13,8 @@ export class Home extends Component {
                 </div>
             
                 <div className="container-fluid">
-                    <div className="mt-2 mb-3 colorBackground">
-                        <div className="row">
+                    <div className="mt- mb-3 colorBackground">
+                        <div className="row-fluid">
                         <p className="text-home text-center d-flex justify-content-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                             Proin ac augue nibh. Nam et interdum augue. Nullam ut ante elit. 
                             Duis elementum tellus nisi, sed aliquet arcu pharetra ut. 
@@ -22,17 +22,18 @@ export class Home extends Component {
                             Praesent in elit viverra, luctus dolor sed, mattis turpis. Nam non sapien mauris. 
                             Nam id nulla ac velit tristique faucibus. Nunc ut nibh posuere, 
                             ultricies augue nec, lacinia sapien. Integer scelerisque faucibus turpis, 
-                            vel ultricies arcu tempus at. </p>
-                        </div>
-                        <div className="row">
-                        <p className="text-home text-center d-flex justify-content-center">Sed tristique tincidunt enim quis viverra. Suspendisse malesuada auctor felis, 
+                            vel ultricies arcu tempus at. 
+
+                            <br></br>
+                            
+                            Sed tristique tincidunt enim quis viverra. Suspendisse malesuada auctor felis, 
                             ac luctus mauris pulvinar vel. Phasellus scelerisque dapibus mauris,
                             eget porta ligula. Vestibulum dictum ullamcorper nunc, nec vehicula tortor dictum.</p>
                         </div>
 
-                        <div className="container-fluid">
+                        <div className="container">
                             <div className="row">
-                                <div className="col-md-6 text-center mb-2 ">
+                                <div className="col-xs-12 col-md-6 text-center mb-2 ">
                                     <div className="card">
                                         <img class="card-img style-card" src='./images/PosterUneAnnonce.png' alt="Poster_une_annonce" />
                                             <div class="card-img-overlay">
