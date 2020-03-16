1dc94b983aff7d6a7434d9a6c9b68b39c3f3981d
shahin321 <shahinchoudhurypro@gmail.com>
2020-03-17 00:30:19 +0100

    Add icon for searchbar #13

--- keyservices/src/components/Layout/Searchbar.js
+++ keyservices/src/components/Layout/Searchbar.js
@@ -8,7 +8,9 @@ export class Searchbar extends Component {
     render() {
         return(
             <Form className="p-2 searchbarHeader" inline>
-                <Button className="searchIconZone"><GoSearch/></Button>
+                <Button className="searchIconZone">
+                    <GoSearch/>
+                </Button>
                 <FormControl type="text" placeholder="Search" className="mr-sm-1 searchFormControl" />
             </Form>
             )
