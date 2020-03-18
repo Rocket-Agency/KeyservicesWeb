5e63451bb0d8ac030d9f0f1f0d88289a9a6a109b
shahin321 <shahinchoudhurypro@gmail.com>
2020-03-18 19:57:45 +0100

    Template react js base fix structure #21

--- src/index.js
+++ src/index.js
@@ -1,39 +1,20 @@
-// Au début de chaque fichier il est important d'importer les différentes dependances dont on aurais besoin !
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
+import { Provider } from 'react-redux';
+import configureStore from './store/store';
+import * as serviceWorker from './serviceWorker';
 
-// Import du plugin de routes
-import { BrowserRouter as Router, Route } from "react-router-dom";
-// Import des composants bootstrap et de la feuille css
-import { Alert } from 'react-bootstrap';
-import './css/index.css';
-// Import des différents composants que nous aurions créés
-import Header from './components/layout/Header';
-import Example1 from './components/examples/Example1.js';
-import Example2 from './components/examples/Example2.js';
-import Example3 from './components/examples/Example3.js';
-import Example4 from './components/examples/Example4.js';
-import Example5 from './components/examples/Example5.js';
 
-import Container from '@material-ui/core/Container';
+const store = configureStore();
 
-// Ici nous allons gérer la structure globale du site, et les différents composants affichés selon les différents chemins
-// Le chemin /exemple2 nous affichera donc le composant Example2
 ReactDOM.render(
-	<Router>
-		<div>
-			<Header />
-			<Container fixed>
-				<div className="test">
-                	<Alert variant="secondary">
-						<Route exact path="/" component={Example1} />
-			        	<Route path="/exemple2" component={Example2} />
-			        	<Route path="/exemple3" component={Example3} />
-			        	<Route path="/exemple4" component={Example4} />
-			        	<Route path="/exemple5" component={Example5} />
-		        	</Alert>
-	        	</div>
-			</Container>
-		</div>
-	</Router>
+    <Provider store={store}>
+        <App />
+    </Provider>
 , document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
---
+++ src/store/store.js
@@ -0,0 +1,16 @@
+import { createStore } from 'redux';
+import rootReducer from '..reducers';
+import initialData from '../initialData';
+
+
+const data = {
+    task: initialData
+};
+
+export default function configureStore(initialState = data) {
+    return createStore(
+        rootReducer,
+        initialState,
+        window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_()
+    );
+};
