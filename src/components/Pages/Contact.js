1dd93eacfff44d100db8ccdb2d6a10a9d9bb9750
shahin321 <shahinchoudhurypro@gmail.com>
2020-03-28 17:02:46 +0100

    Update contact page #33

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -1,11 +1,10 @@
 import React, {Component} from 'react'
 // import { Link } from 'react-router-dom';
 
-import {Form, Col, Container, Row, Button, ListGroup, Card} from 'react-bootstrap';
+import {Form, Col, Container, Row, Button} from 'react-bootstrap';
 import { Visible, Hidden } from 'react-grid-system';
 
 //icons
-import { FaMapMarkerAlt } from 'react-icons/fa';
 import { IoIosArrowForward } from "react-icons/io";
 import { FaArrowRight } from "react-icons/fa";
 
