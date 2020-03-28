1a429b6c79be746e4f9fc18a8ba0bb939ad8f518
shahin321 <shahinchoudhurypro@gmail.com>
2020-03-28 22:25:41 +0100

    Add images and cards propriétaire & locataire #33

--- src/components/Pages/MonEspace.js
+++ src/components/Pages/MonEspace.js
@@ -1,11 +1,47 @@
 import React, {Component} from 'react';
+import '../../css/MonEspace.scss';
+
+import { Col, Container, Row, Card} from 'react-bootstrap';
 
 export class MonEspace extends Component {
 
     render() {
         return(
-            <div className="mt-5 d-flex justify-content-center">
-            <h3>Mon Espace</h3>
+            <div>
+                <div className="mt-5 d-flex justify-content-center">
+                <h3>Mon Espace</h3>
+                </div>
+
+                <Container fluid>
+                    <Row>
+
+                        <Col md={1}></Col>
+
+                        <Col md={5}>
+                            <div className="d-flex justify-content-center">
+                                <div className="card">
+                                    <img class="card-img" id="Propriétaire" src='./images/Propriétaire.png' alt="Gerer_Mes_Locations" />
+                                    <a class="card-footer" href="/">Mon espace propriétaire</a>
+                                </div>
+                            </div>
+                        </Col>     
+
+                        <div className="Vertical-lign align-item-center mt-5"></div>
+
+                        <Col md={5}>
+                            <div className="d-flex justify-content-center"  id="Locataire">
+                                <div className="card">
+                                    <img class="card-img" src='./images/Locataire.png' alt="Gerer_Mes_Locations" />
+                                    <a class="card-footer" href="/">Mon espace locataire</a>
+                                </div>
+                            </div>
+                        </Col>
+
+                        <Col md={1}></Col>
+                    </Row>
+
+
+                </Container>
             </div>
         )
     }
