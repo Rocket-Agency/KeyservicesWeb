a07c779397680c2ae79408c53e2f11f3291638d4
shahin321 <shahinchoudhurypro@gmail.com>
2020-04-05 19:19:20 +0200

    Final desktop keyservices services #42

--- src/components/Pages/Services_conponent.js
+++ src/components/Pages/Services_conponent.js
@@ -1,16 +1,16 @@
 import React, {Component} from 'react';
 import '../../css/Services_component.scss';
 
-import { Col, Container, Row, Form} from 'react-bootstrap';
+import { Col, Container, Row, Form, Card} from 'react-bootstrap';
 
 export class Services_component extends Component {
     render() {
         return(
             <div>
 
-                <h1 className="d-flex justify-content-center">
+                <h2 className="d-flex justify-content-center">
                     Nos services proposés
-                </h1>
+                </h2>
 
 
                 <Container className="blocServices">
@@ -19,13 +19,48 @@ export class Services_component extends Component {
                             <Col sm={6}  className="d-flex justify-content-left blocImgBorder">
                                 <img src='./images/remise_des_clef.png' className="imgServices" alt="Logo keyservices "/>
                             </Col>
-                            <Col sm={6}  className="d-flex justify-content-center">
-                                Texte
+                            <Col sm={6}  className="d-flex justify-content-center blocTextBorder">
+                                <Container>
+                                    <h3 className="d-flex justify-content-center">Remise des clef</h3>
+                                    <p className="d-flex justify-content-center">Ea qui magna laboris labore. Sint sit commodo esse duis deserunt 
+                                        in commodo aliquip ut elit ex dolor adipisicing in. 
+                                        Aliquip deserunt aliqua pariatur aliquip culpa proident
+                                        occaecat ullamco amet dolor.
+                                    </p>
+
+                                    <Container fluid className="mt-2 d-flex justify-content-center">
+                                        <div id="container">                    
+                                            <button className="validation-btn">
+                                                <span className="circle" aria-hidden="true">
+                                                    <span className="icon arrow"></span>
+                                                </span>
+                                                <span className="button-text">En savoir plus</span>
+                                            </button>
+                                        </div>           
+                                    </Container>
+                                </Container>
                             </Col>
                         </Row>
                         <Row className="blocBorderCenter">
-                            <Col sm={6}  className="d-flex justify-content-center">
-                                Texte
+                            <Col sm={6}  className="d-flex justify-content-center blocTextBorder">
+                                <Container>
+                                    <h3 className="d-flex justify-content-center">Remplir son frigo</h3>
+                                    <p className="d-flex justify-content-center">Ea qui magna laboris labore. Sint sit commodo esse duis deserunt 
+                                        in commodo aliquip ut elit ex dolor adipisicing in. 
+                                        Aliquip deserunt aliqua pariatur aliquip culpa proident
+                                        occaecat ullamco amet dolor.
+                                    </p>
+                                    <Container fluid className="mt-2 d-flex justify-content-center">
+                                        <div id="container">                    
+                                            <button className="validation-btn">
+                                                <span className="circle" aria-hidden="true">
+                                                    <span className="icon arrow"></span>
+                                                </span>
+                                                <span className="button-text">En savoir plus</span>
+                                            </button>
+                                        </div>           
+                                    </Container>
+                                </Container>
                             </Col>
                             <Col sm={6}  className="d-flex justify-content-end blocImgBorder">
                                 <img src='./images/remplir_son_frigo.png' width="500px" height="200px" alt="Logo keyservices "/>
@@ -35,13 +70,49 @@ export class Services_component extends Component {
                             <Col sm={6}  className="d-flex justify-content-left blocImgBorder">
                                 <img src='./images/nettoyage.png' width="500px" height="200px" alt="Logo keyservices "/>
                             </Col>
-                            <Col sm={6}  className="d-flex justify-content-center">
-                                Texte
+                            <Col sm={6}  className="d-flex justify-content-center blocTextBorder">
+                                <Container>
+                                    <h3 className="d-flex justify-content-center">Nettoyage</h3>
+                                    <p className="d-flex justify-content-center">Ea qui magna laboris labore. Sint sit commodo esse duis deserunt 
+                                        in commodo aliquip ut elit ex dolor adipisicing in. 
+                                        Aliquip deserunt aliqua pariatur aliquip culpa proident
+                                        occaecat ullamco amet dolor.
+                                    </p>
+                                    <Container fluid className="mt-2 d-flex justify-content-center">
+                                        <div id="container">                    
+                                            <button className="validation-btn">
+                                                <span className="circle" aria-hidden="true">
+                                                    <span className="icon arrow"></span>
+                                                </span>
+                                                <span className="button-text">En savoir plus</span>
+                                            </button>
+                                        </div>           
+                                    </Container>
+                                </Container>
                             </Col>
                         </Row>
-                        <Row className="blocBorderCenter">
-                            <Col sm={6}  className="d-flex justify-content-center">
-                                Texte
+                        <Row className="blocBorderCenterRDV">
+                            <Col sm={6}  className="d-flex justify-content-center blocTextBorder">
+                                <Container>
+                                    <Card.Title>
+                                         <h3 className="d-flex justify-content-center">Prendre rendez-vous pour le checkout</h3>
+                                    </Card.Title>
+                                    <p className="d-flex justify-content-center">Ea qui magna laboris labore. Sint sit commodo esse duis deserunt 
+                                        in commodo aliquip ut elit ex dolor adipisicing in. 
+                                        Aliquip deserunt aliqua pariatur aliquip culpa proident
+                                        occaecat ullamco amet dolor.
+                                    </p>
+                                    <Container fluid className="mt-2 d-flex justify-content-center">
+                                        <div id="container">                    
+                                            <button className="validation-btn">
+                                                <span className="circle" aria-hidden="true">
+                                                    <span className="icon arrow"></span>
+                                                </span>
+                                                <span className="button-text">En savoir plus</span>
+                                            </button>
+                                        </div>           
+                                    </Container>
+                                </Container>
                             </Col>
                             <Col sm={6}  className="d-flex justify-content-end blocImgBorder">
                                 <img src='./images/prise_rdv.png' width="500px" height="200px" alt="Logo keyservices "/>
