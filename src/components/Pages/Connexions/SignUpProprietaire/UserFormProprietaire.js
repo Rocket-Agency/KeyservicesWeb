ea88e65cf9e6516225a4067033da5191c5174d0e
shahin321 <shahinchoudhurypro@gmail.com>
2020-04-07 16:01:24 +0200

    Page d'inscription propriétaire #44

--- src/components/Pages/Connexions/SignUpProprietaire/UserFormProprietaire.js
+++ src/components/Pages/Connexions/SignUpProprietaire/UserFormProprietaire.js
@@ -7,21 +7,52 @@ export class UserFormProprietaire extends Component {
         lastName : '',
         email : '',
         tel : '',
-        user_date_of_birth : ''
+        date_of_birth : ''
+    }
 
+    // Pour aller au formulaire suivant
+    nextStep = () => {
+        const { step } = this.state;
+        this.setState({
+            step: step + 1
+        })
+    }
+
+    // Pour retourner au formulaire précédent
+    provStep = () => {
+        const { step } = this.state;
+        this.setState({
+            step: step - 1
+        });
+    }
+
+    // Handle fields change
+    handleChange = input => e => {
+        this.setState({[input] : e.target.value});
     }
 
     render() {
-        return(
-            <div>
-                <div className="mt-5 d-flex justify-content-center">
-                   <h3>Bienvenue sur votre espace</h3>
-                 </div>
+        const { step } = this.state;
+        const { firstName, lastName, email, tel, date_of_birth} = this.state;
+        const value = { firstName, lastName, email, tel, date_of_birth} = this.state;
 
-
-            </div>
-        )
+       switch(step) {
+           case 1:
+            return (
+                <UserFormProprietaire
+                    nextStep={this.nextStep}
+                    handleChange={this.handleChange}
+                    value={values}
+                />
+            )
+            case 2:
+            return <h1>UserFormProprietairePersonalDetail</h1>
+            case 3:
+            return <h1>Confirm</h1>
+            case 4:
+            return <h1>Succes</h1>
+       }
     }
 }
 
-export default FormProprietairePersonalDetail;
+export default UserFormProprietaire;
