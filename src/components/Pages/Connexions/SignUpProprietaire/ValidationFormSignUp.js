2a98d9ac1d87236206968658aea38f28bf200cd7
shahin321 <shahinchoudhurypro@gmail.com>
2020-04-25 15:05:36 +0200

    Form validation fields #60

---
+++ src/components/Pages/Connexions/SignUpProprietaire/ValidationFormSignUp.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import '../../css/Home.scss';
+
+
+
+export class ValidationFormSignUp extends Component {
+
+    render() {
+        return(
+            <div>
+                test
+            </div>
+        )
+    }
+}
