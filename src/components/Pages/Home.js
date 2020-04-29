4cf837505062af5ffdecc6f4548f1897c6ec9b02
shahin321 <shahinchoudhurypro@gmail.com>
2020-04-29 16:18:31 +0200

    Fix number style #66

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -5,11 +5,13 @@ import '../../css/Home.scss';
 import ProgressiveImage from "react-progressive-graceful-image";
 
 // import ImgDefaultHome from '../../ImagesPlaceholder/placeholderHome.png';
-import PosterUneAnnonceTiny from '../../ImagesPlaceholder/PosterUneAnnonceTiny.png';
-import GererMesLocationsTiny from '../../ImagesPlaceholder/GererMesLocationsTiny.png';
+// import PosterUneAnnonceTiny from '../../ImagesPlaceholder/PosterUneAnnonceTiny.png';
+// import GererMesLocationsTiny from '../../ImagesPlaceholder/GererMesLocationsTiny.png';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 // import { Carousel } from 'react-responsive-carousel';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import Icon from '@mdi/react'
+import { mdiAccount } from '@mdi/js'
 import Carousel from 'react-bootstrap/Carousel'
 
 
@@ -62,7 +64,7 @@ export class Home extends Component {
 
                 <div className="container-fluid p-0 mt-4 mb-4">
                     <div className="colorBackground">
-                    <div className="mt-5 d-flex text-center justify-content-center">
+                        <div className="mt-5 d-flex text-center justify-content-center">
                             <h1>On s’occupe de tous et vous de rien</h1>
                         </div>
                         <div className="row">
@@ -82,59 +84,162 @@ export class Home extends Component {
 
                         <div className="container-fluid">
                             <div className="row">
-                            <div className="col-lg-6 col-md-6 col-sm-12 text-center mb-2">
-                                <div className="card">
-                                    {/* <Img className="card-img style-card" 
-                                        placeholder={ImgDefaultHome} 
-                                        src="./Images/PosterUneAnnonce.png"
-                                        debounce={1000}
-                                        alt="Poster unee annonce" 
-                                    /> */}
-
-                                    <ProgressiveImage  className="card-img style-card" 
-                                        src="./Images/PosterUneAnnonce.png"
-                                        placeholder={PosterUneAnnonceTiny} 
-                                        alt="Poster unee annonce" 
-                                    >
-                                    {src => <img src={src} width="100%" alt="Poster unee annonce"  />}
-                                     </ProgressiveImage>
-                                    <div className="card-img-overlay">
-                                        <button className="btn btn-md btn-success" aria-label="Poster une annonce"> 
-                                            Poster une annonce
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 col-md-6 col-sm-12 text-center mb-2">
+                                {/* <div className="col-lg-6 col-md-6 col-sm-12 text-center mb-2">
                                     <div className="card">
-                                        {/* <Img className="card-img style-card" 
+                                        <Img className="card-img style-card" 
+                                            placeholder={ImgDefaultHome} 
+                                            src="./Images/PosterUneAnnonce.png"
+                                            debounce={1000}
+                                            alt="Poster unee annonce" 
+                                        /> 
+
+                                        <ProgressiveImage  className="card-img style-card" 
+                                            src="./Images/PosterUneAnnonce.png"
+                                            placeholder={PosterUneAnnonceTiny} 
+                                            alt="Poster unee annonce" 
+                                        >
+                                        {src => <img src={src} width="100%" alt="Poster unee annonce"  />}
+                                        </ProgressiveImage>
+                                        <div className="card-img-overlay">
+                                            <button className="btn btn-md btn-success" aria-label="Poster une annonce"> 
+                                                Poster une annonce
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div> */}
+
+                                {/* <div className="col-lg-6 col-md-6 col-sm-12 text-center mb-2">
+                                    <div className="card">
+                                        <Img className="card-img style-card" 
                                             placeholder={ImgDefaultHome} 
                                             src="./Images/GererMesLocations.png"
                                             debounce={1000}
                                             alt="Gerer mes locations" 
-                                        /> */}
+                                        /> 
 
-                                        <Link to="/">
-                                        <ProgressiveImage  className="card-img style-card" 
-                                            src="./Images/GererMesLocations.png"
-                                            placeholder={GererMesLocationsTiny} 
-                                            alt="Poster unee annonce" 
-                                            >
-                                            {src => <img src={src} width="100%" alt="Poster unee annonce"  />}
-                                        </ProgressiveImage>
+                                         <Link to="/">
+                                            <ProgressiveImage  className="card-img style-card" 
+                                                src="./Images/GererMesLocations.png"
+                                                placeholder={GererMesLocationsTiny} 
+                                                alt="Poster unee annonce" 
+                                                >
+                                                {src => <img src={src} width="100%" alt="Poster unee annonce"  />}
+                                            </ProgressiveImage>
                                         </Link>
                                         <div className="card-img-overlay">
                                             <button className="btn btn-md btn-success" aria-label="Gérer mes locations">
                                                 Gérer mes locations
                                             </button>
                                         </div>
-                                   </div>
-                                </div>
+                                    </div>
+                                </div> */}
 
                             </div>
                         </div>
                     </div>
                 </div>
+
+                <section class="section" id="services">
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-12 text-center">
+                                <div class="section-title mb-4 pb-2">
+                                    <h4 class="title mb-4">Business Services</h4>
+                                    <p class="text-muted mx-auto para-desc mb-0">Splash your dream color Bring your home to lively Colors. We make it a priotity to offer flexible services to accomodate your needs</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-lg-6 col-md-6 col-12 mt-4 pt-2">
+                                <div class="service-container shadow rounded p-4 text-center">
+                                    <div class="number-icon position-relative">
+                                        <div class="icon-2 d-inline-block border rounded-pill">  
+                                            <i class="pe-7s-shield text-custom"></i>
+                                        </div>
+
+                                        <div class="number text-center rounded-pill bg-white border">
+                                            <span class="font-weight-bold">01</span>
+                                        </div>
+                                    </div>                           
+
+                                    <div class="content mt-3">
+                                        <h5 class="title text-dark">Finance Management</h5>
+                                        <p class="text-muted">We make it a priotity to offer flexible services to your to lively Colors needs home to lively Colors</p>
+                                        <a href="javascript:void(0)" class="text-dark">
+                                            Read more
+                                            <i class="mdi mdi-chevron-right"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                                                    
+                            <div class="col-lg-6 col-md-6 col-12 mt-4 pt-2">
+                                <div class="service-container shadow rounded p-4 text-center">
+                                    <div class="number-icon position-relative">
+                                        <div class="icon-2 d-inline-block border rounded-pill">  
+                                            <i class="pe-7s-way text-custom"></i>
+                                        </div>
+
+                                        <div class="number text-center rounded-pill bg-white border">
+                                            <span class="font-weight-bold">02</span>
+                                        </div>
+                                    </div>                           
+
+                                    <div class="content mt-3">
+                                         <h5 class="title text-dark">Consulting</h5>
+                                         <p class="text-muted">We make it a priotity to offer flexible services to your to lively Colors needs home to lively Colors</p>
+                                        <a href="javascript:void(0)" class="text-dark">Read more <i class="mdi mdi-chevron-right"></i></a>
+                                     </div>
+                                </div>
+                            </div>
+                                                    
+                            <div class="col-lg-4 col-md-6 col-12 mt-4 pt-2">
+                                                        <div class="service-container shadow rounded p-4 text-center">
+                                                            <div class="number-icon position-relative">
+                                                                <div class="icon-2 d-inline-block border rounded-pill">  
+                                                                    <i class="pe-7s-note2 text-custom"></i>
+                                                                </div>
+
+                                                                <div class="number text-center rounded-pill bg-white border">
+                                                                    <span class="font-weight-bold">03</span>
+                                                                </div>
+                                                            </div>                           
+
+                                                            <div class="content mt-3">
+                                                                <h5 class="title text-dark">Insurance</h5>
+                                                                <p class="text-muted">We make it a priotity to offer flexible services to your to lively Colors needs home to lively Colors</p>
+                                                                <a href="javascript:void(0)" class="text-dark">Read more <i class="mdi mdi-chevron-right"></i></a>
+                                                            </div>
+                                                        </div>
+                            </div>
+                                                    
+                            <div class="col-lg-4 col-md-6 col-12 mt-4 pt-2">
+                                <div class="service-container shadow rounded p-4 text-center">
+                                                            <div class="number-icon position-relative">
+                                                                <div class="icon-2 d-inline-block border rounded-pill">  
+                                                                    <i class="pe-7s-world text-custom"></i>
+                                                                </div>
+
+                                                                <div class="number text-center rounded-pill bg-white border">
+                                                                    <span class="font-weight-bold">04</span>
+                                                                </div>
+                                                            </div>                           
+
+                                                            <div class="content mt-3">
+                                                                <h5 class="title text-dark">Bank Investigate</h5>
+                                                                <p class="text-muted">We make it a priotity to offer flexible services to your to lively Colors needs home to lively Colors</p>
+                                                                <a href="javascript:void(0)" class="text-dark">Read more <i class="mdi mdi-chevron-right"></i></a>
+                                                            </div>
+                                 </div>
+                            </div>
+                                                                                                       
+                            <div class="col-12 mt-4 pt-2 text-center">
+                                 <a href="javascript:void(0)" class="btn btn-pill btn-custom">See More <i class="mdi mdi-chevron-right"></i></a>
+                            </div>
+                        </div>
+                     </div>
+                </section>
             </div>
         )
     }
