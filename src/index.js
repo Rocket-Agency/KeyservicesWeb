181d9aa3db036c88bc414f1092afb5ba46c2a595
shahin321 <shahinchoudhurypro@gmail.com>
2020-05-02 16:53:15 +0200

    Add localstorage part #73

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
@@ -10,9 +11,10 @@ import BreadProvider from './lib/BProvider';
 
 // export default BreadCrumbLib;
 import * as serviceWorker from './serviceWorker';
-import {HashRouter, BrowserRouter} from 'react-router-dom';
 export { Breadcrumb, BreadcrumbItem, BreadProvider }
-ReactDOM.hydrate(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+import { CookiesProvider } from 'react-cookie';
+
+ReactDOM.hydrate(<BrowserRouter><CookiesProvider><App /></CookiesProvider></BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
