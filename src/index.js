99d76ec8fc35dcc0f07163c192cc36725c43cdba
Djamel <dbougouffa@gmail.com>
2020-05-03 06:33:04 +0200

    bug ssr

--- src/index.js
+++ src/index.js
@@ -14,7 +14,9 @@ import * as serviceWorker from './serviceWorker';
 import { CookiesProvider } from 'react-cookie';
 export { Breadcrumb, BreadcrumbItem, BreadProvider }
 
+if (typeof window !== 'undefined') {
 ReactDOM.hydrate(<BrowserRouter><CookiesProvider><App /></CookiesProvider></BrowserRouter>, document.getElementById('root'));
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
