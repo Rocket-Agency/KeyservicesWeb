cc92c7634b4e55d99866b235e5e3247d3a63fbcd
Djamel <dbougouffa@gmail.com>
2020-05-05 15:29:06 +0200

    contact

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -15,8 +15,54 @@ import HomeIcon from '@material-ui/icons/Home';
 import ContactInfos from './ContactInfos';
 import {withRouter} from 'react-router-dom';
 import {Helmet} from "react-helmet";
+import axios from 'axios';
 
 export class Contact extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          first_name: "",
+          last_name: "",
+          email: "",
+          object: "",
+          message: ""
+        };
+    
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(event) {
+        this.setState({
+          [event.target.name]: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        const { first_name, last_name, email, object, message} = this.state;
+        const config = {
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          }
+        }
+        axios.post("http://localhost:3001/api/contact/create",
+            {
+                first_name: first_name,
+                last_name: last_name,
+                email: email,
+                object: object,
+                message: message,
+            },
+            config
+          )
+          .then(response => {
+            this.props.history.push('/dashboard');
+          })
+          .catch(error => {
+          });
+        event.preventDefault();
+      }
     render() {
         return(
             <div>
@@ -46,32 +92,32 @@ export class Contact extends Component {
                                         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                                     </p>
 
-                                    <Form>
+                                    <Form onSubmit={this.handleSubmit}>
                                         <Form.Row>
                                             <Form.Group as={Col} lg="6" md="12" sm="12" controlId="formGridName">
                                             <Form.Label>Nom</Form.Label>
-                                            <Form.Control type="name" placeholder="Entrer votre nom" />
+                                            <Form.Control name="first_name" type="name" placeholder="Entrer votre nom" value={this.state.first_name} onChange={this.handleChange} />
                                             </Form.Group>
                                             
 
                                             <Form.Group as={Col} lg="6" md="12" sm="12" controlId="formGridLastName">
                                             <Form.Label>Prénom</Form.Label>
-                                            <Form.Control type="lastname" placeholder="Entrer votre prénom" />
+                                            <Form.Control name="last_name" type="lastname" placeholder="Entrer votre prénom" value={this.state.last_name} onChange={this.handleChange} />
                                             </Form.Group>
                                         </Form.Row>
 
                                         <Form.Row>
                                             <Form.Group as={Col} lg="6" md="12" sm="12" controlId="formGridEmail">
                                                 <Form.Label>Email</Form.Label>
-                                                <Form.Control type="email" placeholder="Entrer votre adresse email" />
+                                                <Form.Control name="email" type="email" placeholder="Entrer votre adresse email" value={this.state.email} onChange={this.handleChange} />
                                             </Form.Group>
                                             
 
                                             <Form.Group as={Col} controlId="formGridState">
                                                 <Form.Label>Sujet</Form.Label>
-                                                <Form.Control as="select">
+                                                <Form.Control name="object" as="select" value={this.state.object} onChange={this.handleChange}>
                                                     <option>Choisir un sujet...</option>
-                                                    <option>Demande d'informationn</option>
+                                                    <option>Demande d'information</option>
                                                 </Form.Control>
                                                 </Form.Group>
                                         </Form.Row>
@@ -79,7 +125,7 @@ export class Contact extends Component {
                                         <Form.Row>
                                             <Form.Group as={Col}  controlId="votreMessage.ControlTextarea">
                                                 <Form.Label>Votre message</Form.Label>
-                                                <Form.Control as="textarea" rows="3" placeholder="Entrer votre message" />
+                                                <Form.Control name="message" as="textarea" rows="3" placeholder="Entrer votre message" value={this.state.message} onChange={this.handleChange} />
                                             </Form.Group>
                                         </Form.Row>
 
