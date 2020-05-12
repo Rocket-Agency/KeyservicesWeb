50faab4e7494d738397db22604517d7a43da4682
shahin321 <shahinchoudhurypro@gmail.com>
2020-05-12 20:40:10 +0200

    Form inscription ok  #75

--- src/components/Pages/Connexions/SignUpProprietaire/Demo3.js
+++ src/components/Pages/Connexions/SignUpProprietaire/Demo3.js
@@ -1,16 +1,36 @@
 import React, { Component } from "react";
 
 class Demo3 extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: "React"
-    };
-  }
 
-  render() {
-    return <div>This is Demo3 component</div>;
+Search = React.createClass({
+  getInitialState: function() {
+      return { showResults: false };
+  },
+  onClick: function() {
+      this.setState({ showResults: true });
+  },
+  render: function() {
+      return (
+          <div>
+              <input type="submit" value="Search" onClick={this.onClick} />
+              { this.state.showResults ? <Results /> : null }
+          </div>
+      );
   }
+});
+
+Results = React.createClass({
+  render: function() {
+      return (
+          <div id="results" className="search-results">
+              Some Results
+          </div>
+      );
+  }
+});
+
+<Search />
+
 }
 
 export default Demo3;
--- src/components/Pages/Connexions/SignUpProprietaire/InscriptionProprietaire.js
+++ src/components/Pages/Connexions/SignUpProprietaire/InscriptionProprietaire.js
@@ -15,49 +15,7 @@ import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import HomeIcon from '@material-ui/icons/Home';
 import NewsletterForm  from './NewsletterForm';
-import Select from 'react-select';
-
-
-// const ville = [
-//     { title: 'Paris', boolean: true, value :"Paris" },
-//     { title: 'Lyon', boolean: true, value: "Lyon"},
-//     { title: 'Lourdes', boolean: true, value: "Lourdes"},
-//     { title: 'Nice', boolean: false, value: "Nice"},
-//     { title: 'Toulouse', boolean: false, value: "Toulouse"},
-//     { title: 'La Rochelle', boolean: false, value: "La_Rochelle"},
-//   ];
-
-
-//   function choiceCity(ville) {
-//     switch (ville) {
-//       case 0: 'Paris'
-//         return <UserForm />
-//       case 1:
-//         return "Toulouse"
-//       case 2:
-//         return <NewsletterForm />
-//       default:
-//     }
-//   }
-  
-const Selects = [
-    {
-      name: 'firstSelect',
-      options: [
-        {value: 'one', label: '1'},
-        {value: 'two', label: '2'},
-      ],
-    },
-    {
-      name: 'ville',
-      options: [
-        {value: 'Paris', label: 'Paris'},
-        {value: 'Lyon', label: 'Lyon'},
-        {value: 'Lourdes', label: 'Lourdes'},
-      ],
-    },
-  ];
-
+import { Redirect } from 'react-router';
 
 
 export class InscriptionProprietaire extends Component {
@@ -65,129 +23,88 @@ export class InscriptionProprietaire extends Component {
         super(props);
     
         this.state = {
-            sexe:"",
-            firstName: '',
-            lastName: '',
-            tel: '',
-            dateOfBirth: '',
-            factureAdress: '',
-            zipCode: '',
-            email: '',
-            password: '',
-            loading: false,
-            message: "",
-            noneComponent: false,
             newsletterForm: false,
             userForm: false,
+            ville : [
+                { title: 'Paris', boolean: true },
+                { title: 'Lyon', boolean: true} ,
+                { title: 'Lourdes', boolean: true },
+                { title: 'Nice', boolean: false },
+                { title: 'Toulouse', boolean: false },
+                { title: 'La Rochelle', boolean: false },
+            ],
+            cityChoice : "",
         };
     
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this._onButtonClick = this._onButtonClick.bind(this);
+        this.handleChange = this.handleChange.bind(this)
       }
 
-      handleChange(event) {
+
+
+    handleChange(event, value) {    
         this.setState({
-          [event.target.name]: event.target.value
-        });
-      }
+            cityChoice : value.title
+        });  
+        console.log(value)
+    }
     
-      handleSubmit(event) {
-        const { sexe, firstName, lastName, tel, dateOfBirth, factureAdress, zipCode, email, password } = this.state;
-        console.log( sexe, firstName, lastName, tel, dateOfBirth, factureAdress, zipCode, email, password);
-        const config = {
-          headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-          }
-        }
+      handleSubmit(event)  {
+        alert('Votre parfum favori est : ' + this.state.cityChoice);
 
-        this.setState({
-            loading: true
-
+        this.state.ville.map((villesData) => {
+            console.log(this.state.cityChoice)
+            // alert('Votre parfum favori est : ' + villesData.title);
+            if( this.state.cityChoice === villesData.title && villesData.boolean === true) {
+                 this.setState ({
+                    userForm: true 
+                })
+                // if (ville === )
+                return <Redirect to='/inscriptionProprietaire'/>;
+            } else {
+                this.setState ({
+                    newsletterForm : true  
+                })
+                return <Redirect to='/inscriptionProprietaire'/>;
+            }  
         })
-
-        const data = {
-            sexe: sexe,
-            firstName: firstName,
-            lastName: lastName,
-            tel: tel,
-            dateOfBirth: dateOfBirth,
-            factureAdress: factureAdress,
-            zipCode: zipCode,
-            email : email,
-            password : password,
-        }
-
-        axios.post("http://localhost:3001/api/auth/signup", data, config)
-          .then(response => {
-            console.log(response.data);
-            this.setState({
-                loading: false,
-                message: response.data
-            })
-            this.props.history.push('dashboard', {user: response.data});
-          })
-          .catch(error => {
-            console.log(error);
-            this.setState({
-                loading: false
-            })
-          });
         event.preventDefault();
-      }
+    }
+
+        // const data = {
+        //     sexe: sexe,
+        //     firstName: firstName,
+        //     lastName: lastName,
+        //     tel: tel,
+        //     dateOfBirth: dateOfBirth,
+        //     factureAdress: factureAdress,
+        //     zipCode: zipCode,
+        //     email : email,
+        //     password : password,
+        // }
+
+    //     axios.post("http://localhost:3001/api/auth/signup", data, config)
+    //       .then(response => {
+    //         console.log(response.data);
+    //         this.setState({
+    //             loading: false,
+    //             message: response.data
+    //         })
+    //         this.props.history.push('dashboard', {user: response.data});
+    //       })
+    //       .catch(error => {
+    //         console.log(error);
+    //         this.setState({
+    //             loading: false
+    //         })
+    //       });
+    //     event.preventDefault();
+
     
-      loadingShowMsg() {
-        if (this.state.loading) {
-            return <p>Loading...</p>
-        } else {
-            return <p>{this.state.message}</p>
-        }
-      }
 
-      _onButtonClick() {
-        this.setState({
-            noneComponent: false,
-            newsletterForm: true,
-            userForm: true,
-        });
-      }
-
-    //   showComponentForm() {
-    //       if (this.state.hideNewsletterForm || this.state.hideUserForm) {
-    //         if(ville === true) {
-    //             return <UserForm/>
-    //         } else {
-    //             return <NewsletterForm/>
-    //         }
-    //       } else {
-    //           return null
-    //       }
-    //   }
-
-    //   choiceCity() {
-    //       if ("La_Rochelle" === false) {
-    //           return <UserForm />
-    //       } else {
-    //           return <NewsletterForm/>
-    //       }
-    //   }
-
-    onSelectChange(name, value) {
-        let obj = {};
-        obj[name] = value;
-        this.setState(obj);
-      }
-      
 
     render() {
-        const villes = [
-            { title: 'Paris', boolean: true },
-            { title: 'Lyon', boolean: true},
-            { title: 'Lourdes', boolean: true},
-            { title: 'Nice', boolean: false},
-            { title: 'Toulouse', boolean: false},
-          ];
+        const ville = this.state.ville
         return(
             <div>
                 <div className="mt-3 d-flex justify-content-center">
@@ -200,51 +117,50 @@ export class InscriptionProprietaire extends Component {
 
 
                  <Container fluid className="mt-5 d-flex justify-content-center searchCityFields">
-                    <Form.Row className="d-flex justify-content-center">
-                        <Form.Group as={Col} md="2" controlId="formGridCity">
-                            <Autocomplete
-                                id="combo-box-demo"
-                                options={villes}
-                                // getOptionDisabled={(option) => option.title == 'Lourdes'}
-                                getOptionLabel={(option) => option.title}
-                                style={{ width: 300 }}
-                                renderInput={(params) => <TextField {...params} label="Ville" variant="outlined" />}
-                            />
-                        </Form.Group>
-                    </Form.Row>
+                    <Form onSubmit={this.handleSubmit}>
+                        <Form.Row className="d-flex justify-content-center">
+                            <Form.Group as={Col} md="2" controlId="formGridCity">
+                                <Autocomplete
+                                    id="combo-box-demo"
+                                    options={ville}
+                                    // getOptionDisabled={(option) => option.title == 'Lourdes'}
+                                    getOptionLabel={(option) => option.title}
+                                    style={{ width: 300 }}
+                                    onChange={this.handleChange} 
+                                    renderInput={(params) => <TextField {...params}  
+                                    label="Ville" variant="outlined"  />}
+                                />
+                            </Form.Group>
+                        </Form.Row>
+                    
 
-                    <div id="container">                    
-                         <button onClick={this._onButtonClick} className="learn-more" aria-label="Valider la ville">
-                            <span className="circle" aria-hidden="true">
-                                <span className="icon arrow"></span>
-                            </span>
-                             <span className="button-text">Valider la ville</span>
-                        </button>
-                     </div>        
-                                     
+                        <div id="container">                    
+                            <button type="submit" className="learn-more" aria-label="Valider la ville">
+                                <span className="circle" aria-hidden="true">
+                                    <span className="icon arrow"></span>
+                                </span>
+                                <span className="button-text">Valider la ville</span>
+                            </button>
+                        </div>         
+                     </Form>               
                  </Container>
 
 
                     <Container fluid className="Formulaire-incription">
-
-                        { this.state.choiceCity ? <UserForm/> : <NewsletterForm/> }
+                    
+                        {this.state.userForm == true ? (
+                        <UserForm/>
+                    ) : this.state.newsletterForm == true ? (
+                        <NewsletterForm/>
+                    ) : (
+                        null
+                    )}
                     {/* {/* <UserForm />  */}
                     {/* <NewsletterForm />  */}
 
                     {/* <button onClick={this._onButtonClick}>Button</button>
                     {this.state.noneComponent ? <NewsletterForm /> : null} */}
 
-
-                {Selects.map((select, i)=>{
-                        return (
-                            <Select
-                            key={i}
-                            name={select.name}
-                            options={select.options}
-                            onChange={this.onSelectChange.bind(this, select.name)}
-                            />
-                        );
-                        })}
                 </Container>
             </div>
         )
