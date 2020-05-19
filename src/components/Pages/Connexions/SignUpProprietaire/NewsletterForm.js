b092f800c4b9e5d8d1d711eea0cda8f503969abf
Djamel <dbougouffa@gmail.com>
2020-05-19 14:31:16 +0200

    newsletter

--- src/components/Pages/Connexions/SignUpProprietaire/NewsletterForm.js
+++ src/components/Pages/Connexions/SignUpProprietaire/NewsletterForm.js
@@ -1,9 +1,32 @@
 import React, {Component} from 'react';
 import '../../../../css/Newsletter.scss';
 import { Col, Container, Form} from 'react-bootstrap';
+import axios from 'axios';
 
 
 export class NewsletterForm extends Component {
+    constructor (props){
+        super(props);
+
+        this.state = {
+            email : '',
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event){
+        this.setState({email: event.target.value})
+    }
+    handleSubmit(event){
+        axios.post('http://localhost:3001/api/newsletter/create',
+        {
+            email: this.state.email,
+            ville: this.props.value
+        })
+        event.preventDefault();
+    }
 
     render() {
         return(
@@ -13,6 +36,7 @@ export class NewsletterForm extends Component {
                     <p className="InformationForm">Inscrivez votre adresse mail et on vous recontactera dès qu’on sera dans votre zone</p>
                 </div>
                 <Container fluid className="mt-2 d-flex justify-content-center searchCityFields">
+                    <form onSubmit={this.handleSubmit}>
                     <Form.Row className="d-flex justify-content-center">
                         <Form.Group as={Col} md="4" controlId="formGridId">
                             <Form.Label>Entrer votre adresse mail</Form.Label>
@@ -20,8 +44,8 @@ export class NewsletterForm extends Component {
                             name="email" 
                             type="email" 
                             placeholder="Entrer votre adresse mail" 
-                            // value={this.state.email} 
-                            // onChange={this.handleChange} 
+                            value={this.state.email} 
+                            onChange={this.handleChange} 
                         />
                         </Form.Group>
                     </Form.Row>
@@ -33,7 +57,8 @@ export class NewsletterForm extends Component {
                             </span>
                              <span className="button-text">Valider votre email</span>
                         </button>
-                     </div>                        
+                     </div>
+                     </form>                        
                  </Container>
 
             </div>
