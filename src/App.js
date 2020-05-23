14d481ff0d33eb4e4157795fc8e008372e1260f3
Djamel <dbougouffa@gmail.com>
2020-05-23 12:56:15 +0200

    not found

--- src/App.js
+++ src/App.js
@@ -38,29 +38,10 @@ import ProfileTabs from './components/Pages/Dashboard';
 //Deconnexion
 import Deconnexion from './components/Pages/Molecule/Deconnexion';
 import ConnexionConcierge from './components/AppKeyservicesGestion/ConnexionConcierge';
+import NotFound from './components/Pages/NotFound';
 
 //Connexion
 // import ConnexionC from './components/Mobile/ConnexionConcierge'
-
-//Error
-const NotFound = () => {  
-  return (    
-    <div>
-       <BreadcrumbItem to="/"><HomeIcon/>Home</BreadcrumbItem>
-        <BreadcrumbItem>Erreur 404</BreadcrumbItem>
-        <div id="notfound">
-          <div class="notfound">
-            <div class="notfound-404">
-              <h1>404</h1>
-            </div>
-            <h2>Oops! Cette page n'a pas pu être trouvée</h2>
-              <p>Désolé mais la page que vous cherchez n'existe pas, a été supprimée. nom modifié ou est temporairement indisponible.</p>
-              <Link to="/">Retour à la page d'accueil</Link>   
-          </div>
-        </div>
-    </div>
-    );
-  };
 
 function App() {
   return (
