7ab1fc4e301d838f1055b21992145aaf370a5d68
Djamel <dbougouffa@gmail.com>
2020-05-23 14:26:11 +0200

    sitemap

--- src/App.js
+++ src/App.js
@@ -1,47 +1,15 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
 import { BreadProvider } from './index';
-import { BreadcrumbItem } from './index';
-import { Link } from 'react-router-dom';
-import HomeIcon from '@material-ui/icons/Home';
 import './App.scss';
 import './index.css';
 
 //Import component for Header & Footer
 import Header from './components/Layout/Header';
 import Footer from './components/Layout/Footer';
-import CGU from './components/Pages/CGU';
 
 import FilAriane from './components/Layout/FilAriane';
 
-//Import pages
-import {Home} from './components/Pages/Home';
-import KeyservicesPresentation from './components/Pages/KeyservicesPresentation';
-
-import {Contact} from './components/Pages/Contact';
-
-import {ValidationContactForm} from './components/Pages/Molecule/ValidationContactForm';
-import {MonEspace} from './components/Pages/MonEspace';
-
-//Connexion pages
-import {LoginProprietaire} from './components/Pages/Connexions/Login/LoginProprietaire';
-import {LoginLocataire} from './components/Pages/Connexions/Login/LoginLocataire';
-
-import {ForgetPassword} from './components/Pages/Connexions/Profiles/ForgetPassword';
-
-//Page d'inscription
-import {InscriptionProprietaire} from './components/Pages/Connexions/SignUpProprietaire/InscriptionProprietaire';
-
-//Dashboard
-import ProfileTabs from './components/Pages/Dashboard';
-
-//Deconnexion
-import Deconnexion from './components/Pages/Molecule/Deconnexion';
-import ConnexionConcierge from './components/AppKeyservicesGestion/ConnexionConcierge';
-import NotFound from './components/Pages/NotFound';
-
-//Connexion
-// import ConnexionC from './components/Mobile/ConnexionConcierge'
+import Routing from './Route'
 
 function App() {
   return (
@@ -52,26 +20,8 @@ function App() {
               <div className="content-wrap">
               <Header />
               <FilAriane />
-              
-                <Switch>
-                  <Route path='/' component={Home} exact />
-                  <Route path='/presentation' component={KeyservicesPresentation} exact/>
-                  <Route path='/contact' component={Contact} exact/>
-                  <Route path='/validationContactForm' component={ValidationContactForm} exact/>
-                  <Route path='/monEspace' component={MonEspace} exact/>
-                  <Route path='/loginProprietaire' component={LoginProprietaire} exact/>
-                  <Route path='/loginLocataire' component={LoginLocataire} exact/>
-                  <Route path='/inscriptionProprietaire' component={InscriptionProprietaire} />
-                  <Route path='/forgetPassword' component={ForgetPassword} />
-                  <Route path='/dashboard' component={ProfileTabs} />
-                  <Route path='/deconnexion' component={Deconnexion} />
-                  <Route path='/cgu' component={CGU} />
-                  <Route path='/loginConcierge' component={ConnexionConcierge} />
-                  <Route component={NotFound} />
-                </Switch>
-
+              <Routing/>
               </div>
-
               <Footer/>
           </div>
           </>
---
+++ src/Route.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import { Switch, Route } from 'react-router';
+
+//Import component for Header & Footer
+import CGU from './components/Pages/CGU';
+
+//Import pages
+import {Home} from './components/Pages/Home';
+import KeyservicesPresentation from './components/Pages/KeyservicesPresentation';
+
+import {Contact} from './components/Pages/Contact';
+
+import {ValidationContactForm} from './components/Pages/Molecule/ValidationContactForm';
+import {MonEspace} from './components/Pages/MonEspace';
+
+//Connexion pages
+import {LoginProprietaire} from './components/Pages/Connexions/Login/LoginProprietaire';
+import {LoginLocataire} from './components/Pages/Connexions/Login/LoginLocataire';
+
+import {ForgetPassword} from './components/Pages/Connexions/Profiles/ForgetPassword';
+
+//Page d'inscription
+import {InscriptionProprietaire} from './components/Pages/Connexions/SignUpProprietaire/InscriptionProprietaire';
+
+//Dashboard
+import ProfileTabs from './components/Pages/Dashboard';
+
+//Deconnexion
+import Deconnexion from './components/Pages/Molecule/Deconnexion';
+import ConnexionConcierge from './components/AppKeyservicesGestion/ConnexionConcierge';
+import NotFound from './components/Pages/NotFound';
+
+export default class Routing extends Component {
+
+    render(){
+        return(
+          <Switch>
+              <Route path='/' component={Home} exact />
+              <Route path='/presentation' component={KeyservicesPresentation} exact/>
+              <Route path='/contact' component={Contact} exact/>
+              <Route path='/validationContactForm' component={ValidationContactForm} exact/>
+              <Route path='/monEspace' component={MonEspace} exact/>
+              <Route path='/loginProprietaire' component={LoginProprietaire} exact/>
+              <Route path='/loginLocataire' component={LoginLocataire} exact/>
+              <Route path='/inscriptionProprietaire' component={InscriptionProprietaire} />
+              <Route path='/forgetPassword' component={ForgetPassword} />
+              <Route path='/dashboard' component={ProfileTabs} />
+              <Route path='/deconnexion' component={Deconnexion} />
+              <Route path='/cgu' component={CGU} />
+              <Route path='/loginConcierge' component={ConnexionConcierge} />
+              <Route component={NotFound} />
+            </Switch>
+        )
+    }
+}
