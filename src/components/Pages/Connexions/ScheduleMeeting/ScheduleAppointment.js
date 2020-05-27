6e2fdd29a21fb1d9cd11850e2c32b423c54ba66b
shahin321 <shahinchoudhurypro@gmail.com>
2020-05-27 03:21:34 +0200

    Add file for appointment #133

--- src/components/Pages/Connexions/ScheduleMeeting/ScheduleAppointment.js
+++ src/components/Pages/Connexions/ScheduleMeeting/ScheduleAppointment.js
@@ -5,6 +5,10 @@ import { Container, Row,  Col } from 'react-bootstrap';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import BasicDateTimePicker from './../../BasicDateTimePicker';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+import TabTimeChoice from './TabTimeChoice';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,29 +20,39 @@ const useStyles = makeStyles((theme) => ({
         width: theme.spacing(16),
       },
   },
+  rootTab: {
+    flexGrow: 1,
+  },
 }));
 
 export default function SimplePaper() {
   const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
 
   return (
     <>
       <Grid container spacing={5} className={classes.root}>
-            <Grid item xs={12} sm={6} className={classes.root}>
+
+            <TabTimeChoice />
+
+            {/* <Grid item sm={6} className={classes.root}>
                 <Button variant="contained" color="primary" disableElevation > 
                         Matin
                 </Button>
             </Grid>
-            <Grid item xs={12} sm={6} className={classes.root}>
+            <Grid item  sm={6}  className={classes.root}>
                 <Button variant="contained" color="primary" disableElevation>
                     Après-Midi
                 </Button>
             </Grid>
             <Grid item xs={12} sm={6}>
                 <BasicDateTimePicker />
-            </Grid>
-
-
+            </Grid> */}
         </Grid>
     </>
   );
