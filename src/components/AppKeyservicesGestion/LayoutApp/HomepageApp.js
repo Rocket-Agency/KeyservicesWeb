b1ab624249722b0efa841840f3fe89ae09e7d9af
laxsan <s.navalaxsan@gmail.com>
2020-05-27 19:12:34 +0200

    modification mineur sur l'aplication

--- src/components/AppKeyservicesGestion/LayoutApp/HomepageApp.js
+++ src/components/AppKeyservicesGestion/LayoutApp/HomepageApp.js
@@ -11,21 +11,46 @@ export class HomepageApp extends Component {
             this.state = {
                 user_first_name: "",
                 user_last_name: "",
+                userid: '',
+                accessToken: '',
+                group: '',
             }
+            this.userid = '';
+            this.token = '';
+            this.group = '';
         }
     
         componentDidMount(){
+            if(localStorage.getItem('token') && localStorage.getItem('id') && localStorage.getItem('group')){
+                this.userid = localStorage.getItem('id');
+                this.token = localStorage.getItem('token');
+                this.group = localStorage.getItem('group');
+              }
+              else{
+                localStorage.setItem('token', this.props.location.state.user.accessToken)
+                localStorage.setItem('id', this.props.location.state.user.id)
+                localStorage.setItem('group', this.props.location.state.user.groups)
+                this.userid = this.props.location.state.user.id;
+                this.token = this.props.location.state.user.accessToken;
+                this.group = this.props.location.state.user.groups;
+              }
+
+              if(this.group != 'GROUP_CONCIERGE'){
+                  this.props.history.push('/loginConcierge');
+                  localStorage.clear();
+              }
+              
             const config = {
                 headers: {
-                  'x-access-token': this.props.token
+                  'x-access-token': this.token
                 }
               }
-              axios.get(`http://localhost:3001/photo/getPhotoByUserId/`+ this.props.userid)
-              .then(res => {
-                this.setState( { photo_url : res.data.photo_url} );
-              })
+            //   axios.get(`http://localhost:3001/photo/getPhotoByUserId/`+ this.userid)
+            //   .then(res => {
+            //     this.setState( { photo_url : res.data.photo_url} );
+            //   })
           
-              axios.get(`http://localhost:3001/api/user/`+ this.props.userid, config)
+              axios.get(`http://localhost:3001/api/user/`+ this.userid, config)
                 .then(res => {
                   this.setState( { user_first_name : res.data.user_first_name});
                   this.setState( { user_last_name: res.data.user_last_name });
@@ -35,11 +60,19 @@ export class HomepageApp extends Component {
         }
 
     render(){
+        const { 
+            user_first_name , user_last_name
+          } = this.state;
+        const values = { user_first_name,user_last_name };
+
+        console.log(this.token);
         return (
             <div>
                 <Container >
                     <Row className="d-flex justify-content-center">
-                        <HeaderApp />
+                        <HeaderApp 
+                        values={values}
+                        />
                     </Row> 
                 </Container>
                 
